aaea04e8b1f4d985ec22026a65622bf783c25c26
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-13 10:46:37 -0800

    Updated routes; added home controller; added index and about pages to home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root "home#index"
-  get "/about" => "about#index"
+  get "/about" => "home#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
