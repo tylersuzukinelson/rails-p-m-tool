2926b607261626bf9ef159e161b601961440b5cf
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-20 12:27:52 -0800

    Added create action for task controller.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,23 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def create
+    @task = Task.new permitted_params
+    if @task.save
+      redirect_to @task
+    else
+      redirect_to new_task_path, notice: error_message
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.require('task').permit(:title, :description)
+  end
+
+  def error_message
+    @task.errors.full_messages.join('; ')
+  end
+
 end
