ff2466fdd3beb08e3733d8dfc1b225d44b5641c8
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-20 14:43:43 -0800

    Added project model, projects controller, and index action of projects controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "home#index"
   get "/about" => "home#about"
   resources :tasks
+  resources :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
