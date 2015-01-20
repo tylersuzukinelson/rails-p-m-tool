975d0905bec9de0fb4da728434f2c9a421fc74a4
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-20 13:30:08 -0800

    Added validations to tasks controller.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ActiveRecord::Base
+  validates :title, presence: true
+  validate :default_completion
+
+  private
+
+  def default_completion
+    self.completed ||= false
+  end
 end
