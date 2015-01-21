54d85411cdfdb4954a972244954b5f8900156917
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-20 16:21:19 -0800

    Added create action to projects controller.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,8 +4,27 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def create
+    @project = Project.new permitted_params
+    if @project.save
+      redirect_to @project
+    else
+      redirect_to new_project_path, notice: error_messages
+    end
+  end
+
   def new
     @project = Project.new
+  end
+
+  private
+
+  def permitted_params
+    params.require(:project).permit(:title, :body)
+  end
+
+  def error_messages
+    @project.errors.full_messages.join('; ')
   end
 
 end
