e05a98f587d5862f6a88c40056e993df709a8053
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-21 20:17:45 -0800

    Added create action to comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new comment_params
+    @comment.discussion = Discussion.find params[:discussion_id]
+    if @comment.save
+      redirect_to @comment.discussion
+    else
+      redirect_to @comment.discussion, notice: error_messages
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def error_messages
+    @comment.errors.full_messages.join('; ')
+  end
+
 end
