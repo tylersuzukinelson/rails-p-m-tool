bc1f3754dd78ef6b7e0c54add0b6f75d0bf056cd
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-21 20:05:40 -0800

    Added create action to discussions controller, and added list of discussions to projects show page

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,2 +1,23 @@
 class DiscussionsController < ApplicationController
+
+  def create
+    @discussion = Discussion.new discussion_params
+    @discussion.project = Project.find params[:project_id]
+    if @discussion.save
+      redirect_to @discussion.project
+    else
+      redirect_to @discussion.project, notice: error_messages
+    end
+  end
+
+  private
+
+  def discussion_params
+    params.require(:discussion).permit(:title, :body)
+  end
+
+  def error_messages
+    @discussion.errors.full_messages.join('; ')
+  end
+
 end
